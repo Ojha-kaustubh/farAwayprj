419505febfeea3ce4a6207e881ef332d565e9929
Ojha-kaustubh <kaustubha07ojha@gmail.com>
2025-02-10 22:55:34 +0530

    checkbox are added

--- src/App.js
+++ src/App.js
@@ -12,12 +12,23 @@ export default function App() {
     setItems((items) => items.filter((item) => item.id !== id));
   }
 
-  
+  function handleToggleItem(id) {
+    setItems((items) =>
+      items.map((item) =>
+        item.id === id ? { ...item, packed: !item.packed } : item
+      )
+    );
+  }
+
   return (
     <div className="app">
       <Logo />
       <Form onAddItem={handleAddItem} />
-      <PackingList items={items} onDeleteItem={handleRemoveItem} />
+      <PackingList
+        items={items}
+        onDeleteItem={handleRemoveItem}
+        onToggleItem={handleToggleItem}
+      />
       <Stats />
     </div>
   );
@@ -67,19 +78,25 @@ function Form({ onAddItem }) {
   );
 }
 
-function PackingList({ items, onDeleteItem }) {
+function PackingList({ items, onDeleteItem, onToggleItem }) {
   return (
     <ul className="List">
       {items.map((item) => (
-        <Item item={item} key={item.id} onDeleteItem = {onDeleteItem} />
+        <Item
+          item={item}
+          key={item.id}
+          onDeleteItem={onDeleteItem}
+          onToggleItem={onToggleItem}
+        />
       ))}
     </ul>
   );
 }
 
-function Item({ item , onDeleteItem}) {
+function Item({ item, onDeleteItem , onToggleItem}) {
   return (
     <li>
+      <input type="checkbox" value={item.packed} onChange={() => onToggleItem(item.id)} />
       <span style={item.packed ? { textDecoration: "line-through" } : {}}>
         {item.quantity}
         {item.description}
